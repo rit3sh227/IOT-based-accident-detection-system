e8c7323122210ec8313813fc62110883eb49b609
rit3sh227 <ritesh281020@gmail.com>
2026-09-15 13:32:31 +0530

    Add basic Arduino working example

---
+++ arduino_working.cpp
@@ -0,0 +1,10 @@
+// used to check the working of arduino 
+void setup() {
+  Serial.begin(9600);
+  Serial.println("ARDUINO UNO IS WORKING");
+}
+
+void loop() {
+  Serial.println("Arduino running...");
+  delay(1000);
+}
